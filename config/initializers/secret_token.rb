cba8bd933d897a5d2049323180943f7f89be807f
prashant85 <dixitprashant855@gmail.com>
2012-11-07 10:38:01 +0530

    first commit on mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'fb5833eba2d143943fc2a927a1103c8acc2aaf30c4cf49cc1f4b95f85fe3ca93ab9d20ce2b86ec067d6d5a78d4f116486947532b79959c79497e96cca546dec1'
